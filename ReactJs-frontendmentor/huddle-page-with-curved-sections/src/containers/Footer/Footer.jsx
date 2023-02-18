6e0b5a13ab287466a613f8cc0072e61024a4c4b5
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-18 11:31:02 +0300

    Finished footer

--- ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/Footer/Footer.jsx
+++ ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/Footer/Footer.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { logo } from '../../assets/images/Images';
+import {
+  FooterBtn,
+  FooterLeft,
+  FooterSection,
+  Form,
+  Input,
+  NewsLetter,
+  Socials,
+} from './Footer.style';
+
+import { MdPhoneInTalk, MdEmail } from 'react-icons/md';
+import { FaFacebookSquare, FaTwitterSquare } from 'react-icons/fa';
+import { SiInstagram } from 'react-icons/si';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <FooterSection>
+      <FooterLeft>
+        <img src={logo} alt='logo' />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla
+          quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+        </p>
+        <p>
+          <span>
+            <MdPhoneInTalk />
+          </span>
+          Phone:+1-23-567-896
+        </p>
+        <p>
+          <span>
+            <MdEmail />
+          </span>
+          example@email.com
+        </p>
+
+        <Socials>
+          <FaFacebookSquare />
+          <SiInstagram />
+          <FaTwitterSquare />
+        </Socials>
+      </FooterLeft>
+
+      <NewsLetter>
+        <h2>NEWSLETTER</h2>
+        <p>
+          To receive tips on how to grow your community, sign up to our weekly
+          newsletter. We'll never send you spam or pass on your email address
+        </p>
+
+        <Form action='post'>
+          <Input type='text' placeholder='Enter Your Email' />
+          <FooterBtn for='text'>Subscribe</FooterBtn>
+        </Form>
+      </NewsLetter>
+    </FooterSection>
+  );
 }
 
 export default Footer;
--- ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/Footer/Footer.style.jsx
+++ ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/Footer/Footer.style.jsx
@@ -1,1 +1,123 @@
 import styled from 'styled-components';
+
+import {
+  bgFooterTopDesktop,
+  bgFooterTopMobile,
+} from '../../assets/images/Images';
+
+export const FooterSection = styled.footer`
+  display: flex;
+  flex-direction: row;
+  padding: 3rem 5rem;
+  justify-content: space-between;
+  color: ${({ theme }) => theme.colors.VeryPaleBlue};
+  background-color: ${({ theme }) => theme.colors.VeryDarkCyan};
+  /* 
+  background-image: url('${bgFooterTopDesktop}');
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: 50px 150px; */
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    padding: 1rem 2rem;
+    flex-direction: column-reverse;
+  }
+`;
+
+export const FooterLeft = styled.footer`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  img {
+    width: 150px;
+    margin-bottom: 1rem;
+    color: white;
+  }
+  p {
+    font-family: ${({ theme }) => theme.fonts.openSans};
+    font-weight: 400;
+    font-size: 1.2rem;
+    display: flex;
+    margin: 0.5rem 0;
+    span {
+      font-size: 1.5rem;
+      margin-right: 1rem;
+      cursor: pointer;
+    }
+  }
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    padding: 5rem 0;
+  }
+`;
+
+export const Socials = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+  width: 30%;
+  margin-top: 3rem;
+  cursor: pointer;
+  transition: all 300ms ease-in-out;
+`;
+
+export const NewsLetter = styled.div`
+  max-width: 400px;
+  h2 {
+    font-family: ${({ theme }) => theme.fonts.openSans};
+    font-size: 2rem;
+    font-weight: 400;
+  }
+  p {
+    max-width: 250px;
+    font-family: ${({ theme }) => theme.fonts.openSans};
+    font-weight: 400;
+    font-size: 1.2rem;
+    display: flex;
+    margin: 0.5rem 0;
+  }
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    padding-top: 5rem;
+    p {
+      max-width: 100%;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 2rem;
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    flex-direction: column;
+    align-items: flex-end;
+  }
+`;
+
+export const Input = styled.input`
+  padding: 0.5rem 1rem;
+  margin-right: 2rem;
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.White};
+  border: none;
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    margin-bottom: 10px;
+    margin-right: 0;
+    width: 100%;
+  }
+`;
+
+export const FooterBtn = styled.button`
+  padding-right: 2rem;
+  padding-left: 2rem;
+  background-color: ${({ theme }) => theme.colors.Pink};
+  border: none;
+  border-radius: 5px;
+  color: inherit;
+  cursor: pointer;
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    padding: 0.5rem 1rem;
+    /* width: 100px; */
+  }
+`;
