5e2d9ca39ea39ba0bd23280168e56624c9e5f435
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-18 10:01:07 +0300

    Finishe CTA

--- ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/CTA/CTA.jsx
+++ ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/CTA/CTA.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Btn from '../../components/Button/Btn';
+import { CTASection } from './CTA.style';
 
 function CTA() {
-  return <div>CTA</div>;
+  return (
+    <CTASection>
+      <h1>Ready To Build Your Community?</h1>
+      <Btn />
+    </CTASection>
+  );
 }
 
 export default CTA;
--- ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/CTA/CTA.style.jsx
+++ ReactJs-frontendmentor/huddle-page-with-curved-sections/src/containers/CTA/CTA.style.jsx
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+export const CTASection = styled.section`
+  padding: 3rem 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 4rem 0;
+
+  h1 {
+    font-family: ${({ theme }) => theme.fonts.poppins};
+    font-size: 2.5rem;
+    font-weight: 600;
+    margin-bottom: 2rem;
+    color: ${({ theme }) => theme.colors.VeryDarkCyan};
+  }
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    padding: 1rem 2rem;
+    h1 {
+      text-align: center;
+    }
+  }
+`;
