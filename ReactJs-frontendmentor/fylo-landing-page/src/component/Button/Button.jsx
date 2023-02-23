5e7ce0ec96a03095b0f344aed024c7577e296d78
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-23 14:10:58 +0300

    Added button component

---
+++ ReactJs-frontendmentor/fylo-landing-page/src/component/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledButton } from './Button.style';
+
+function Button({ text }) {
+  return <StyledButton>{text}</StyledButton>;
+}
+
+export default Button;
---
+++ ReactJs-frontendmentor/fylo-landing-page/src/component/Button/Button.style.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  background-color: ${(props) => props.theme.colors.BrightBlue};
+  color: ${(props) => props.theme.colors.LightGrayishBlue};
+  font-family: ${(props) => props.theme.fonts.openSans};
+  font-weight: 400;
+  padding: 1rem 3rem;
+  border: none;
+  border-radius: 3px;
+  transition: all 300ms ease-in-out;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
