a624f6b9424b0ba1652a1f7f6047723add27f845
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-23 14:11:49 +0300

    Fixied issues in navbar

--- ReactJs-frontendmentor/fylo-landing-page/src/component/Navbar/Navbar.style.jsx
+++ ReactJs-frontendmentor/fylo-landing-page/src/component/Navbar/Navbar.style.jsx
@@ -54,17 +54,22 @@ export const StyledUl = styled.ul`
 export const StyledSingInBtn = styled.button`
   background-color: transparent;
   color: ${(props) => props.theme.colors.VeryDarkBlue};
-  border: none;
+  border: 2px solid #fff;
   font-size: 1.6rem;
   font-family: ${(props) => props.theme.fonts.Raleway};
   font-weight: 400;
   cursor: pointer;
   transition: all 300ms ease-in-out;
+  padding: 1rem 2rem;
+
   &:hover {
+    border: 2px solid ${(props) => props.theme.colors.LightGray};
+    border-radius: 5px;
     color: ${(props) => props.theme.colors.DesaturatedBlue};
   }
 
   @media ${({ theme }) => theme.breakPoints.mobile} {
     font-size: 1.4rem;
+    padding: 0;
   }
 `;
