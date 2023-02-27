678d234d024b2209f18d8834c6c56f78c1e99ee5
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-27 08:42:40 +0300

    Finished Footer

--- ReactJs-frontendmentor/fylo-landing-page/src/containers/Footer/Footer.jsx
+++ ReactJs-frontendmentor/fylo-landing-page/src/containers/Footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
-import { SectionFooter } from './Footer.style';
+import { logo } from '../../assets/images/images';
+import { BsFillTelephoneFill, BsTwitter, BsInstagram } from 'react-icons/bs';
+import { IoIosMail } from 'react-icons/io';
+import { FaFacebookF } from 'react-icons/fa';
+import {
+  SectionFooter,
+  StyledContact,
+  StyledFooter,
+  StyledIconContainer,
+  StyledUL,
+} from './Footer.style';
 
 function Footer() {
-  return <SectionFooter>Footer</SectionFooter>;
+  return (
+    <SectionFooter>
+      <img src={logo} alt='logo' />
+      <StyledFooter>
+        <StyledContact>
+          <p>
+            <BsFillTelephoneFill /> Phone: +1-234-567-895
+          </p>
+          <p>
+            <IoIosMail /> example@fylo.com
+          </p>
+        </StyledContact>
+        <StyledUL>
+          <li>About Us</li>
+          <li>Jobs</li>
+          <li>Press</li>
+          <li>Blog</li>
+        </StyledUL>
+        <StyledUL>
+          <li>Contact Us</li>
+          <li>Terms</li>
+          <li>Privacy</li>
+        </StyledUL>
+        <StyledIconContainer>
+          <div>
+            <FaFacebookF />
+          </div>
+          <div>
+            <BsTwitter />
+          </div>
+          <div>
+            <BsInstagram />
+          </div>
+        </StyledIconContainer>
+      </StyledFooter>
+    </SectionFooter>
+  );
 }
 
 export default Footer;
--- ReactJs-frontendmentor/fylo-landing-page/src/containers/Footer/Footer.style.jsx
+++ ReactJs-frontendmentor/fylo-landing-page/src/containers/Footer/Footer.style.jsx
@@ -2,8 +2,97 @@ import styled from 'styled-components';
 
 export const SectionFooter = styled.footer`
   padding: 3rem 5rem;
+  background-color: ${({ theme }) => theme.colors.VeryDarkBlue};
+  color: ${({ theme }) => theme.colors.White};
+
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 150px;
+  }
 
   @media ${({ theme }) => theme.breakPoints.mobile} {
     padding: 1.5rem;
+    img {
+      margin-top: 2rem;
+    }
+  }
+`;
+
+export const StyledUL = styled.ul`
+  list-style: none;
+  width: 350px;
+
+  li {
+    margin-bottom: 1rem;
+    cursor: pointer;
+    transition: all 300ms ease-in-out;
+    &:hover {
+      color: ${({ theme }) => theme.colors.BrightBlue};
+    }
+  }
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    width: 250px;
+    margin-top: 2rem;
+  }
+`;
+
+export const StyledFooter = styled.div`
+  display: flex;
+  margin-top: 1rem;
+  font-family: ${({ theme }) => theme.fonts.openSans};
+  font-weight: 400;
+  font-size: 1.4rem;
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    flex-direction: column;
+  }
+`;
+
+export const StyledContact = styled.div`
+  width: 350px;
+  p {
+    display: flex;
+    align-items: center;
+    margin-top: 1.5rem;
+    svg {
+      width: 15px;
+      margin-right: 1rem;
+    }
+  }
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    width: 250px;
+  }
+`;
+
+export const StyledIconContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  div {
+    width: 30px;
+    height: 30px;
+    border: 1px solid ${({ theme }) => theme.colors.White};
+    border-radius: 50%;
+    display: grid;
+    place-items: center;
+    transition: all 300ms ease-in-out;
+    cursor: pointer;
+
+    svg {
+      font-size: 1.6rem;
+    }
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.BrightBlue};
+      border: 1px solid ${({ theme }) => theme.colors.BrightBlue};
+    }
+  }
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    justify-content: center;
+    margin: 2rem 0;
   }
 `;
