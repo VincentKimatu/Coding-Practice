1fa017716e888ab1169732eb3486f1b99f5a2a5b
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-03-04 13:05:37 +0300

    finished workflow section

--- ReactJs-frontendmentor/clipboard-landing-page/src/container/workflow/Workflow.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/workflow/Workflow.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import data from '../../constant/data';
+import SectionData from '../../component/sectionData/SectionData';
+
+import { StyledWrapper, WorkflowSection } from './Workflow.style';
+import { workflow } from '../../constant/content';
+import WorkflowWrapper from '../../component/workflowWrapper/WorkflowWrapper';
 
 function Workflow() {
-  return <div>Workflow</div>;
+  return (
+    <WorkflowSection>
+      <SectionData title={data.title.three} paragraph={data.paragraph.three} />
+
+      <StyledWrapper>
+        {workflow.map((item, id) => (
+          <WorkflowWrapper item={item} key={id} />
+        ))}
+      </StyledWrapper>
+    </WorkflowSection>
+  );
 }
 
 export default Workflow;
--- ReactJs-frontendmentor/clipboard-landing-page/src/container/workflow/Workflow.style.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/workflow/Workflow.style.jsx
@@ -1,1 +1,16 @@
 import styled from 'styled-components';
+
+export const WorkflowSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+  margin-bottom: 5rem;
+`;
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem;
+  justify-content: center;
+`;
