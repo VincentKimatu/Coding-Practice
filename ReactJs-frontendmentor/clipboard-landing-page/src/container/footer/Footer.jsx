88f8a8e2c48795c6e4690b6de4a13db29a4092f7
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-03-06 09:35:33 +0300

    Finished footer section

--- ReactJs-frontendmentor/clipboard-landing-page/src/container/footer/Footer.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/footer/Footer.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Logo } from '../../assets/images/images';
+import { ImFacebook2, ImTwitter } from 'react-icons/im';
+import { SiInstagram } from 'react-icons/si';
+import { SocialsWrapper, StyledFooter, StyledLinks } from './Footer.style';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <StyledFooter>
+      <img src={Logo} alt='logo' />
+
+      <StyledLinks>
+        <div>
+          <li>FAQs</li>
+          <li>Contact Us</li>
+        </div>
+        <div>
+          <li>Privacy Policy</li>
+          <li>Press Kit</li>
+        </div>
+        <li>Install Guide</li>
+      </StyledLinks>
+
+      <SocialsWrapper>
+        <div>
+          <ImFacebook2 />
+        </div>
+        <div>
+          <ImTwitter />
+        </div>
+        <div>
+          <SiInstagram />
+        </div>
+      </SocialsWrapper>
+    </StyledFooter>
+  );
 }
 
 export default Footer;
--- ReactJs-frontendmentor/clipboard-landing-page/src/container/footer/Footer.style.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/footer/Footer.style.jsx
@@ -1,1 +1,70 @@
 import styled from 'styled-components';
+
+export const StyledFooter = styled.footer`
+  background-color: ${(props) => props.theme.colors.GrayishBlue};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 4rem 5rem;
+  margin-top: 10rem;
+
+  img {
+    width: 7rem;
+  }
+
+  @media ${(props) => props.theme.breakPoints.mobile} {
+    flex-direction: column;
+    img {
+      width: 5rem;
+    }
+  }
+`;
+
+export const StyledLinks = styled.ul`
+  display: flex;
+  list-style: none;
+  margin-right: 10rem;
+  font-size: 1.6rem;
+  gap: 5rem;
+
+  li {
+    color: ${(props) => props.theme.colors.DarkGrayishBlue};
+    font-weight: 600;
+    padding: 0.5rem 0;
+    transition: all 300ms ease-in-out;
+    cursor: pointer;
+    &:hover {
+      color: ${(props) => props.theme.colors.StrongCyan};
+    }
+  }
+
+  @media ${(props) => props.theme.breakPoints.tablet} {
+    margin-right: 3rem;
+  }
+
+  @media ${(props) => props.theme.breakPoints.mobile} {
+    font-size: 1.4rem;
+    flex-direction: column;
+    gap: 0;
+    padding: 2rem 0;
+    margin-right: 0;
+
+    li {
+      text-align: center;
+    }
+  }
+`;
+
+export const SocialsWrapper = styled.div`
+  display: flex;
+  gap: 1.5rem;
+
+  div {
+    transition: all 300ms ease-in-out;
+    cursor: pointer;
+    &:hover {
+      color: ${(props) => props.theme.colors.StrongCyan};
+    }
+  }
+`;
