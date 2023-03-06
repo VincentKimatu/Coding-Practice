8deca35db4c8bba20c7953db2e453528e2356330
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-03-06 08:57:34 +0300

    Finished OS section

--- ReactJs-frontendmentor/clipboard-landing-page/src/container/os/OS.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/os/OS.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import data from '../../constant/data';
+import Button from '../../component/button/Button';
+import SectionData from '../../component/sectionData/SectionData';
+import { StyledOSSection } from './OS.style';
 
 function OS() {
-  return <div>OS</div>;
+  return (
+    <StyledOSSection>
+      <SectionData title={data.title.four} paragraph={data.paragraph.four} />
+      <Button />
+    </StyledOSSection>
+  );
 }
 
 export default OS;
--- ReactJs-frontendmentor/clipboard-landing-page/src/container/os/OS.style.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/os/OS.style.jsx
@@ -1,1 +1,8 @@
 import styled from 'styled-components';
+
+export const StyledOSSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 5rem;
+`;
