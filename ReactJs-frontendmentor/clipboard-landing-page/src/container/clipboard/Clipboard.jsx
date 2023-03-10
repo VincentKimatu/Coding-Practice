6f59578395b4adb57bd53dec982d7dea8d356ad5
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-03-10 08:19:37 +0300

    Finished clipboardsection

--- ReactJs-frontendmentor/clipboard-landing-page/src/container/clipboard/Clipboard.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/clipboard/Clipboard.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { ImageDevice } from '../../assets/images/images';
+import { ClipboardSection, StyledImage } from './Clipboard.style';
+import SectionData from '../../component/sectionData/SectionData';
+import data from '../../constant/data';
 
 function Clipboard() {
-  return <div>Clipboard</div>;
+  return (
+    <ClipboardSection>
+      <SectionData title={data.title.two} paragraph={data.paragraph.two} />
+      <StyledImage>
+        <img src={ImageDevice} alt='mobile and tablet device' />
+      </StyledImage>
+    </ClipboardSection>
+  );
 }
 
 export default Clipboard;
--- ReactJs-frontendmentor/clipboard-landing-page/src/container/clipboard/Clipboard.style.jsx
+++ ReactJs-frontendmentor/clipboard-landing-page/src/container/clipboard/Clipboard.style.jsx
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+export const ClipboardSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8rem;
+  margin-top: 10rem;
+
+  @media ${(props) => props.theme.breakPoints.mobile} {
+    gap: 5rem;
+    margin-top: 6rem;
+  }
+`;
+
+export const StyledImage = styled.div`
+  width: 70vw;
+  img {
+    width: 100%;
+  }
+
+  @media ${(props) => props.theme.breakPoints.mobile} {
+    width: 90vw;
+  }
+`;
